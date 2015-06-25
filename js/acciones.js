de4183e28fa4dbfdc77ce8f2b4d6b9cf1998c7ac
AL20F13 <duranc96@gmail.com>
2015-06-25 09:09:17 -0500

    dos

--- js/acciones.js
+++ js/acciones.js
@@ -2,6 +2,51 @@
 $(document).ready(function(e) {
 document.addEventListener("deviceready",function(){
 	
+	$('#btnmenu').on('tap',function() {
+		navigator.vibrate(1000);
+	}); //tap a btnmenu
+	
+	$('#btninicio').on('tap',function() {
+		navigator.vibrate(1000);
+	}); //tap a btninicio
+	
+	$('#btnchimpance').on('tap',function() {
+		navigator.notification.beep(1);
+		}); //tap a btnchimpance
+		
+		$('#btnpestañear').on('tap',function() {
+		navigator.notification.beep(1);
+		}); //tap a btnpestañear
+		
+		$('#btngallina').on('tap',function() {
+		navigator.notification.beep(1);
+		}); //tap a btngallina 
+		
+		$('#btnfuerza').on('tap',function() {
+		navigator.notification.beep(1);
+		}); //tap a btnfuerza
+		
+		$('#btnestomago').on('tap',function() {
+		navigator.notification.beep(1);
+		}); //tap a btnestomago
+
+       $('#btnbacterias').on('tap',function() {
+		navigator.notification.beep(1);
+		}); //tap a btnbacterias
+
+       $('#btndormir').on('tap',function() {
+		navigator.notification.beep(1);
+		}); //tap a btndormir
+		
+		$('#btncelulas').on('tap',function() {
+		navigator.notification.beep(1);
+		}); //tap a btncelulas
+
+       $('#btnsencible').on('tap',function() {
+		navigator.notification.beep(1);
+		}); //tap a btnsencible
+
+	
 }); 
 });
 
